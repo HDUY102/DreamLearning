9a6b7dcb6bdca18307f8bc5406cf970f8ec89b97
HDUY <leduy020702@gmail.com>
2024-01-20 16:40:38 +0700

    try to use shacn

---
+++ app/register/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+
+const Register = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Register
